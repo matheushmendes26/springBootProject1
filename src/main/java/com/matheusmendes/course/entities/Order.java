f2da31a7dcf8f5a9d6b875d4a328414a54bde7f5
Matheus Mendes <matheush.1998@hotmail.com>
2023-03-28 12:31:23 -0300

    OrderStatusEnum

--- src/main/java/com/matheusmendes/course/entities/Order.java
+++ src/main/java/com/matheusmendes/course/entities/Order.java
@@ -1,6 +1,7 @@
 package com.matheusmendes.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.matheusmendes.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -24,6 +25,9 @@ public class Order implements Serializable {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant date;
+
+    private Integer orderStatus;
+
     //Implementation of associations
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -31,9 +35,21 @@ public class Order implements Serializable {
 
     public Order(){}
 
-    public Order(Long id, Instant date, User client) {
+    public Order(Long id, Instant date, User client, OrderStatus orderStatus) {
         this.id = id;
         this.date = date;
         this.client = client;
+        setOrderStatus(orderStatus);
+    }
+
+
+    public OrderStatus getOrderStatus(){
+        return OrderStatus.valueOf(orderStatus);
+    }
+    public void setOrderStatus(OrderStatus orderStatus){
+        if(orderStatus !=null){
+            this.orderStatus = orderStatus.getCode();
+        }
+
     }
 }
